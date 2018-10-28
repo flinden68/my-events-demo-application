d9eb8474cc8b5ef7bf8858434a703fc1f8524372
Frank van der Linden <flinden68@elstarit.nl>
2018-10-28 08:27:46 +0100

    Refactor the EventListContainer to an object Made the CRUD work

--- react-app/src/js/actions/index.js
+++ react-app/src/js/actions/index.js
@@ -1,3 +1,5 @@
-import { ADD_EVENT, DELETE_EVENT } from "../constants/action-types";
+import { ADD_EVENT, DELETE_EVENT, GET_ALL_EVENTS } from "../constants/action-types";
 export const addEvent = event => ({ type: ADD_EVENT, payload: event });
 export const deleteEvent = event => ({ type: DELETE_EVENT, payload: event });
+
+export const getAllEvents = events => ({ type: DELETE_EVENT, payload: events });
--- react-app/src/js/constants/action-types.js
+++ react-app/src/js/constants/action-types.js
@@ -1,4 +1,4 @@
 
 export const ADD_EVENT = "ADD_EVENT";
 export const UPDATE_EVENT = "UPDATE_EVENT";
-export const DELETE_EVENT = "DELETE_EVENT";
+export const GET_ALL_EVENTS = "GET_ALL_EVENTS";
