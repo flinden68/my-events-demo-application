93fe5929494329de6be49c92049aa8eb83824db2
Frank van der Linden <flinden68@elstarit.nl>
2018-10-30 13:42:28 +0100

    Added CRUD API Added Cors protection

--- nodejs-api/server.js
+++ nodejs-api/server.js
@@ -1,49 +1,28 @@
 let express = require('express');
 let bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 const app = express();
+var cors = require('cors');
+const apiRoutes = require('./routes/api-routes');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Connection URL
-const mongoUrl = 'mongodb://localhost:27017/';
-const dbName = 'events-store';
 var port = process.env.PORT || 3030;
 
-var db;
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-MongoClient.connect(mongoUrl, (err, client) => {
-    if (err) return console.log(err)
-    db = client.db(dbName) // whatever your database name is
-    app.listen(port, () => {
-        console.log("Running Event Service API on port " + port);
-    })
-})
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.post('/api/event/create', (req, res) => {
-    console.log(req.body);
-    console.log(req.body.start_date);
-        /*db.collection('event').insertOne(req.body, (err, result) => {
-            if (err) return console.log(err);
+app.use('/api', apiRoutes);
 
-            console.log('saved to database');
-            res.redirect('/');
-        })*/
-});
-
-app.get('/api/events', (req, res) => {
-    db.collection('event').find().toArray(function(err, results) {
-        console.log(results)
-        res.send(results);
-    })
-});
-
-/*app.listen(port, function () {
+app.listen(port, function () {
     console.log("Running Event Service API on port " + port);
-});*/
+});
 
