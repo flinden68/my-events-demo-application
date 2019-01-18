384221aa6b89050bf42f83fcc01bc50413e4ecc4
Frank van der Linden <flinden68@elstarit.nl>
2019-01-18 21:48:00 +0100

    Fixed a styling issue on the table

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,
-    { enableTracing: true } // <-- debugging purposes only
+    { enableTracing: false } // <-- debugging purposes only
   )],
   exports: [RouterModule]
 })
