a3fa856250f01e8dbd36204a6718fdb79eb896fc
Frank van der Linden <flinden68@elstarit.nl>
2019-01-23 13:09:09 +0100

    Fixed a typo in the EN language file Added multi origin CORS support

--- nodejs-api/server/server.ts
+++ nodejs-api/server/server.ts
@@ -3,31 +3,25 @@ import * as dotenv from 'dotenv';
 import * as bodyParser from 'body-parser';
 import * as homeController from './controller/home.controller';
 import {ApiRoutes} from "./routes/api-routes";
+const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
 
-app.use(function (req, res, next) {
+const whitelist = ['http://localhost:8080','http://localhost:8081','http://localhost:4200'];
 
-    const allowedOrigins = ['http://localhost:8080','http://localhost:8081','http://localhost:4200'];
-    //let origin = req.headers.origin;
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+const corsOptions = {
+    origin: function (origin:any, callback:any) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    // Pass to next layer of middleware
-    next();
-});
-
+app.use(cors(corsOptions));
 app.set('port', process.env.PORT || 3536);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
