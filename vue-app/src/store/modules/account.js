18d9c150df8eab74928719d5e5e58ccab6ee2f6d
Frank van der Linden <flinden68@elstarit.nl>
2019-01-30 16:23:43 +0100

    Added edit Event, Add Event and Login

--- vue-app/src/store/modules/account.js
+++ vue-app/src/store/modules/account.js
@@ -2,13 +2,18 @@ import accountApi from "../../service/account";
 
 // state
 const state = {
-    current: null
+    current: undefined,
+    authenticated: false
 }
 
 // getters
 const getters = {
     getCurrent: (state) => {
         return state.current
+    },
+
+    isAuthenticated : (state) => {
+        return state.authenticated
     }
 }
 
@@ -16,59 +21,81 @@ const getters = {
 const actions = {
     create(commit, account){
         accountApi.createAccount(account)
-            .subscribe( response => {
+            .then( response => {
                 commit('create', response.data)
             })
     },
     update(commit, account){
         accountApi.updateAccount(account._id, account)
-            .subscribe( response => {
+            .then( response => {
                 commit('update', response.data)
             })
     },
     delete(commit, account){
         accountApi.deleteAccount(account)
-            .subscribe( response => {
+            .then( response => {
                 commit('delete', response.data)
+            })
+    },
+    fetchAccount({commit}, account){
+
+        accountApi.fetchAccountById(account._id)
+            .then( response => {
+                if(response.status == 204){
+                    commit('hasNoAccount', null, false)
+                }else{
+                    if(response.data.email != account.email){
+                        commit('hasNoAccount', null, false)
+                    }else {
+                        commit('hasAccount', response.data, true)
+                    }
+                }
             })
     },
     fetchById(commit, account){
         accountApi.fetchAccountById(account._id)
-            .subscribe( response => {
+            .then( response => {
                 commit('fetch', response.data)
             })
     },
     fetchByEmail(commit, account){
         accountApi.fetchAccountByEmail(account.email)
-            .subscribe( response => {
+            .then( response => {
                 commit('fetch', response.data)
             })
     },
-    logout(commit){
+    logout({commit}){
         commit('logout');
     }
 }
 
 //mutations
 const mutations = {
-    create({state}, action){
+    create(state, action){
         state.current = action;
     },
 
-    update({state}, action){
+    update(state, action){
         state.current = action;
     },
 
-    delete({state}){
+    delete(state){
         state.current = null;
     },
 
-    fetch({state}, action){
-        state.current = action;
+    hasAccount(state, account){
+        state.current = account;
+        state.authenticated = true
     },
 
-    logout({state}){
+    hasNoAccount(state, account){
+        state.current = account;
+        state.authenticated = false
+    },
+
+    logout(state){
         state.current = null;
+        state.authenticated = false
     }
 
 
